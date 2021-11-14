a5b5a65fc3a81f8075129f1196894eb5e73c308a
Douglas Andrade <douglas.andrade@qintess.com>
2021-11-13 22:04:33 -0300

    Primeira classe: Posicao

--- src/Programa.java
+++ src/Programa.java
@@ -2,8 +2,9 @@
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Posicao pos = new Posicao (3,5);
+		System.out.println (pos);
 
 	}
-
 }
